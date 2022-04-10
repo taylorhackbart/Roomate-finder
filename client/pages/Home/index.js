8b8a00ce9ea574fb09644cc398aa8517cb141b73
Taylor <taylor.hackbart@du.edu>
2022-04-10 14:40:15 -0700

    switched to react-native front end environment

---
+++ client/pages/Home/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function NoMatch() {
+  return (
+    <>
+      <h1 style={{ textAlign: "center", marginBottom: "50px" }}>
+        {" "}
+        404 PAGE NOT FOUND{" "}
+      </h1>
+      <p> But my name is Taylor</p>
+    </>
+  );
+}
+export default NoMatch;
